af22f8cf6fd79e1c605a909f72aaaba4f2286b7f
Scott Zinski <zoolunk@gmail.com>
2020-10-18 12:10:36 -0400

    add total price calc

--- js/controllers/CartController.js
+++ js/controllers/CartController.js
@@ -1,4 +1,12 @@
 app.controller('CartController', ['$scope', function ($scope) {}]);
+    $scope.totalPrice = function (items) {
+      const total = items.reduce((acc, item) => {
+        let { price, qty } = item;
+        return acc + +price * +qty;
+      }, 0);
+      return total.toFixed(2).toString();
+    };
+
     $scope.removeItem = function (id) {
       $scope.cartItems = $scope.cartItems.filter(item=> item._id !== id)
     };
