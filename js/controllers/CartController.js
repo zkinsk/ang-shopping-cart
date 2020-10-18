d70c8d2fe39c78f6c54636609eb06375e7443787
Scott Zinski <zoolunk@gmail.com>
2020-10-18 13:12:10 -0400

    fix error with quantity being a string

--- js/controllers/CartController.js
+++ js/controllers/CartController.js
@@ -8,7 +8,7 @@ app.controller('CartController', [
         blurb: 'A mouse with no tail',
         img: 'mouse.jpeg',
         price: '30.42',
-        qty: '1',
+        qty: 1,
         currency: '$',
       },
       {
@@ -17,7 +17,7 @@ app.controller('CartController', [
         blurb: 'Sit way back from your work',
         img: 'wirelesskeyboard.webp',
         price: '60.03',
-        qty: '1',
+        qty: 1,
         currency: '$',
       },
       {
@@ -26,7 +26,7 @@ app.controller('CartController', [
         blurb: 'Seeds for spitting',
         img: 'seeds.jpeg',
         price: '5.32',
-        qty: '1',
+        qty: 1,
         currency: '$',
       },
       {
@@ -35,24 +35,31 @@ app.controller('CartController', [
         blurb: 'Get your lean on',
         img: 'chair.jpg',
         price: '500.44',
-        qty: '1',
+        qty: 1,
         currency: '$',
       },
     ];
-    $scope.salesTax = 0.05;
+    $scope.salesTaxRate = 0.05;
+    $scope.salesTax = 0;
     $scope.shippingCosts = 10.0;
     $scope.imgPrefix = 'assets/images/';
 
     $scope.totalPrice = function (items) {
-      const total = items.reduce((acc, item) => {
-        let { price, qty } = item;
-        return acc + +price * +qty;
-      }, 0);
-      return total.toFixed(2).toString();
+      const total = calcTotal(items);
+      $scope.salesTax = total * $scope.salesTaxRate;
+      return (total + $scope.salesTax).toFixed(2).toString();
     };
 
     $scope.removeItem = function (id) {
       $scope.cartItems = $scope.cartItems.filter((item) => item._id !== id);
     };
+    $scope.disabled = 'disabled';
   },
 ]);
+
+function calcTotal(itemsArr) {
+  return itemsArr.reduce((acc, { price, qty }) => {
+    typeof qty === 'number' || (qty = 0);
+    return acc + +price * qty;
+  }, 0);
+}
