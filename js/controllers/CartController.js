37d393e95efed2fc19e34a4b22e8413696e4e359
Scott Zinski <zoolunk@gmail.com>
2020-10-18 12:00:39 -0400

    add remove item functionality

--- js/controllers/CartController.js
+++ js/controllers/CartController.js
@@ -1,1 +1,6 @@
 app.controller('CartController', ['$scope', function ($scope) {}]);
+    $scope.removeItem = function (id) {
+      $scope.cartItems = $scope.cartItems.filter(item=> item._id !== id)
+    };
+  },
+]);
