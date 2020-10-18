be37583bd9a93b7c433c75a73f527d48a6c0d609
Scott Zinski <zoolunk@gmail.com>
2020-10-18 12:11:06 -0400

    add item data

--- js/controllers/CartController.js
+++ js/controllers/CartController.js
@@ -1,4 +1,48 @@
-app.controller('CartController', ['$scope', function ($scope) {}]);
+app.controller('CartController', [
+  '$scope',
+  function ($scope) {
+    $scope.cartItems = [
+      {
+        _id: 'asgsdfjosidf120223',
+        name: 'Wireless Mouse',
+        blurb: 'A mouse with no tail',
+        img: 'mouse.jpeg',
+        price: '30.42',
+        qty: '1',
+        currency: '$',
+      },
+      {
+        _id: 'asgsdfjosidf120123',
+        name: 'Wireless Keyboard',
+        blurb: 'Sit way back from your work',
+        img: 'wirelesskeyboard.webp',
+        price: '60.03',
+        qty: '1',
+        currency: '$',
+      },
+      {
+        _id: 'asgsdfjosidf1adfss',
+        name: 'Sunflower Seeds',
+        blurb: 'Seeds for spitting',
+        img: 'seeds.jpeg',
+        price: '5.32',
+        qty: '1',
+        currency: '$',
+      },
+      {
+        _id: 'asgsdfjosidfsad3222',
+        name: 'Desk Chair',
+        blurb: 'Get your lean on',
+        img: 'chair.jpg',
+        price: '500.44',
+        qty: '1',
+        currency: '$',
+      },
+    ];
+    $scope.salesTax = 0.05;
+    $scope.shippingCosts = 10.0;
+    $scope.imgPrefix = 'assets/images/';
+
     $scope.totalPrice = function (items) {
       const total = items.reduce((acc, item) => {
         let { price, qty } = item;
